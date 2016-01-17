1570593ad1ca78bab97025d3e62d5971c7e7a1d1
Allmantool <Allmantool@gmail.com>
2016-01-18 01:17:50 +0300

    18.01.2016 H

--- NaftanRailway.WebUI/App_Start/BundleConfig.cs
+++ NaftanRailway.WebUI/App_Start/BundleConfig.cs
@@ -25,18 +25,12 @@ namespace NaftanRailway.WebUI {
                 .Include("~/Scripts/UI_user.js"));
 
             bundles.Add(new StyleBundle("~/Content/cssbundle")
-                .Include("~/Content/font/glyphicons-halflings-regular.woff",new CssRewriteUrlTransform())
-                .Include("~/Content/bootstrap.min.css",
-                         "~/Content/bootstrap-theme.min.css",
+                .Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform())
+                .Include("~/Content/bootstrap-theme.min.css",
                          "~/Content/bootstrap-datetimepicker.min.css",
                          "~/Content/Bootstrap_AutoComplete.css",
                          "~/Content/jquery.ui.theme.css",
                          "~/Content/jquery.ui.theme.font-awesome.css",
-                         "~/Content/font/glyphicons-halflings-regular.eot",
-                         "~/Content/font/glyphicons-halflings-regular.svg",
-                         "~/Content/font/glyphicons-halflings-regular.ttf",
-                         "~/Content/font/glyphicons-halflings-regular.woff",
-                         "~/Content/font/glyphicons-halflings-regular.woff2",
                          "~/Content/ErrorStyles.css"));
         }
     }
