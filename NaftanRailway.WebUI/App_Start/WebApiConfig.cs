5895a841a4cb551bde00f1818549abf0e9d49056
Allmantool <allmantool@gmail.com>
2016-05-06 15:35:06 +0300

    06.05.2016 W

--- NaftanRailway.WebUI/App_Start/WebApiConfig.cs
+++ NaftanRailway.WebUI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@
 namespace NaftanRailway.WebUI {
     public static class WebApiConfig {
         public static void Register(HttpConfiguration config) {
+            //config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
