5af0c86e9326e714a292d1c3137cd010ea67f3db
Allmantool <allmantool@gmail.com>
2016-09-09 14:53:35 +0300

    09.09.2016 W

---
+++ NaftanRailway.Domain/BusinessModels/AuthorizationLogic/CustomPrincipal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+
+namespace NaftanRailway.Domain.BusinessModels.AuthorizationLogic {
+    //public class CustomPrincipal:WindowsPrincipal {
+
+    //}
+}
