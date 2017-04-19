51f226d9ba1fa8d9b16861fcdf561d0e630f3090
Allmantool <allmantool@gmail.com>
2017-04-19 13:54:24 +0300

    19.04.2017 W

--- NaftanRailway.WebUI/Scripts/IEJs/IE9-.js
+++ NaftanRailway.WebUI/Scripts/IEJs/IE9-.js
@@ -23,7 +23,7 @@
 //    }
 //}
 
-//add console functionality
+//polyfill console functionality
 var alertFallback = true;
 if (typeof console === "undefined" || typeof console.log === "undefined") {
     console = {};
@@ -34,4 +34,25 @@ if (typeof console === "undefined" || typeof console.log === "undefined") {
     } else {
         console.log = function () { };
     }
+}
+
+//polyfill for array indexOf functionality
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (elt /*, from*/) {
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+             ? Math.ceil(from)
+             : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++) {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
 }
--- NaftanRailway.WebUI/Startup.cs
+++ NaftanRailway.WebUI/Startup.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Transports;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(NaftanRailway.WebUI.Startup))]
 namespace NaftanRailway.WebUI {
     public class Startup {
         public void Configuration(IAppBuilder app) {
+            //var heartBeat = GlobalHost.DependencyResolver.Resolve<ITransportHeartbeat>();
+            //var monitor = new PresenceMonitor(heartBeat);
+
+            //monitor.StartMonitoring();
+
+            GlobalHost.Configuration.DefaultMessageBufferSize = 100;
             app.MapSignalR();
             //GlobalHost.HubPipeline.RequireAuthentication();
         }
