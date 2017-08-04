a4c62bf8a9418d0145592bc88dd15c3ebdfa2f8b
Allmantool <allmantool@gmail.com>
2017-08-04 16:52:11 +0300

    04.08.2017 W

--- NaftanRailway.BLL/DTO/General/ITreeNode.cs
+++ NaftanRailway.BLL/DTO/General/ITreeNode.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace NaftanRailway.BLL.DTO.General {
     /// <summary>
@@ -11,7 +9,7 @@ namespace NaftanRailway.BLL.DTO.General {
         int Id { get; set; }
         string Name { get; }
         T Parent { get; set; }
-        IList<T> Children { get; }
+        IList<T> Children { get;  }
 
         /// <summary>
         /// True if a Leaf Node
@@ -32,98 +30,5 @@ namespace NaftanRailway.BLL.DTO.General {
 
         void AddChild(T child);
         void AddChildren(IEnumerable<T> children);
-    }
-
-    /// <summary>
-    /// it represents a generic abstract(base) class for hierarchy tree node implementions
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public abstract class TreeNodeBase<T> : ITreeNode<T> where T : class, ITreeNode<T> {
-        /// <summary>
-        /// It returns context ('this'). ('this' couldn't straight cast to type T).
-        /// Implemention in conrete class must override this property and to return
-        /// ( get { return this; } )
-        /// </summary>
-        protected abstract T MySelf { get; }
-
-        public TreeNodeBase(string name) {
-            Name = name;
-            Children = new List<T>();
-        }
-
-        public int Id {
-            get {
-                throw new NotImplementedException();
-            }
-
-            set {
-                throw new NotImplementedException();
-            }
-        }
-        public string Name {
-            get; private set;
-        }
-        /// <summary>
-        /// the parent has a public setter due to the interface
-        /// </summary>
-        public T Parent {
-            get; set;
-        }
-        public IList<T> Children {
-            get; private set;
-        }
-
-        public bool IsLeaf {
-            get {
-                return Children.Count == 0;
-            }
-        }
-
-        public bool IsRoot {
-            get {
-                return Parent == null;
-            }
-        }
-
-        public void AddChild(T child) {
-            child.Parent = MySelf;
-            Children.Add(child);
-        }
-        public void AddChildren(IEnumerable<T> children) {
-            foreach (T child in children)
-                AddChild(child);
-        }
-
-        /// <summary>
-        /// List of Leaf Nodes
-        /// </summary>
-        /// <returns></returns>
-        public List<T> GetLeafNodes() {
-            return Children.Where(x => x.IsLeaf).ToList();
-        }
-        public List<T> GetNonLeafNodes() {
-            return Children.Where(x => !x.IsLeaf).ToList();
-        }
-
-        /// <summary>
-        /// Get the Root Node of the Tree
-        /// </summary>
-        /// <returns></returns>
-        public T GetRootNode() {
-            if (Parent == null)
-                return MySelf;
-
-            return Parent.GetRootNode();
-        }
-
-        /// <summary>
-        /// Dot separated name from the Root to this Tree Node
-        /// </summary>
-        /// <returns></returns>
-        public string GetFullyQualifiedName() {
-            if (Parent == null) return Name;
-
-            return string.Format("{0}.{1}", Parent.GetFullyQualifiedName(), Name);
-        }
     }
 }
--- NaftanRailway.UnitTests/General/HierarchyTree.cs
+++ NaftanRailway.UnitTests/General/HierarchyTree.cs
@@ -1,54 +1,151 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NaftanRailway.BLL.DTO.General;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
 
 namespace NaftanRailway.UnitTests.General {
+
+    public class TreeNode : TreeNodeBase<TreeNode>, ITreeNode<TreeNode> {
+        public TreeNode(string name) : base(name) {
+            Debug.Write(name);
+        }
+
+        protected override TreeNode MySelf {
+            get {
+                return this;
+            }
+        }
+    }
+
     [TestClass]
     public class HierarchyTree {
         [TestMethod]
         public void SetUp() {
+            const int countGroup = 3;
+            var startPeriod = new DateTime(2017, 1, 1);
 
-            //            --The order must be same in each aggregation functions
-            //; WITH grSubResult AS(
-            //    SELECT[grNumber] = DENSE_RANK() OVER(ORDER BY kn.KEYKRT DESC),
+            var hirearchyDict = new Dictionary<int, string>{
+                { 0, "Карточка" },
+                { 1, "Перечень" }
+            };
 
-            //           [rankInGr] = RANK() OVER(partition by kn.KEYKRT ORDER BY kn.KEYKRT DESC, knos.id_kart desc),
+            var conectString = ConfigurationManager.AppSettings["TestLocalConnection"] ??
+                @"data source = CPN8\HOMESERVER; initial catalog = Railway; integrated security = True; Trusted_Connection = Yes;";
 
-            //           [treeLevel] = GROUPING_ID(kn.KEYKRT, kn.NKRT, knos.id_kart),
-            //           --[level_card] = GROUPING(knos.id_kart),
 
-            //           [scroll] = kn.NKRT, kn.KEYKRT,
+            #region Query 
+            /* 04.08.2017
+            * It query converts flatted table to hierachy table (The hierachy deep is defined by group predicate)
+            * 
+            * [elementId] - primary key
+            * [parentId] - parents element id
+            * [groupId] - group id
+            * [rankInGr] - element primary key in group
+            * [treeLevel] - height of tree
+            * [levelName] - custom group tree node name
+            * [searchkey] - key for search in plane (source table)
+            * [label] - description for rendering purpose
+            */
+            //--Warning weakness!
+            //--The order must be same in each aggregation functions
+            var query = $@";WITH grSubResult AS (
+                SELECT  [elementId] = ROW_NUMBER() OVER(ORDER BY kn.KEYKRT DESC, knos.id_kart desc),
+                        [groupId] = DENSE_RANK() OVER(ORDER BY kn.KEYKRT DESC),
+                        [rankInGr] = RANK() OVER(partition by kn.KEYKRT ORDER BY kn.KEYKRT DESC, knos.id_kart desc),
+                        [treeLevel] = GROUPING_ID(kn.KEYKRT, kn.NKRT, knos.id_kart),
+                        --[level_card] = GROUPING(knos.id_kart),
+                        [scroll] = kn.NKRT, kn.KEYKRT,
+                        [card] = knos.NKRT, knos.id_kart,
+                        [count] = COUNT(*)
+                FROM [unCharge] AS knos INNER JOIN [listOfCards] AS kn
+                    ON kn.KEYKRT = knos.keykrt
+                WHERE knos.tdoc > 0 AND kn.DTBUHOTCHET >= '{startPeriod.ToString("d")}'
+                GROUP BY GROUPING SETS(
+                        --(),
+                        (kn.KEYKRT, kn.NKRT),
+                        (kn.KEYKRT, kn.NKRT, knos.id_kart, knos.nkrt)
+	                )
+                )
 
-            //           [card] = knos.NKRT, knos.id_kart,
+                SELECT 
+                    [parentId] = CASE [elementId] when MAX(elementId) OVER (PARTITION BY KEYKRT) THEN 0 ELSE MAX(elementId) OVER (PARTITION BY KEYKRT) END,
+	                [elementId], [groupId], [rankInGr], [treeLevel],
+	                [levelName] = CASE [treeLevel] WHEN 0 THEN N'{hirearchyDict[0]}' WHEN 1 THEN N'{hirearchyDict[1]}' ELSE NULL END,
+	                [searchkey] = CASE [treeLevel] WHEN 0 THEN [id_kart] WHEN 1 THEN [keykrt] ELSE NULL END,
+	                [label] = CASE [treeLevel] WHEN 0 THEN [card] WHEN 1 THEN CONVERT(NVARCHAR(max),[scroll]) ELSE NULL END
+                FROM grSubResult
+                WHERE [treeLevel] IN ( 1, 0) and [groupId] <= {countGroup}
+                ORDER BY KEYKRT DESC, id_kart desc;";
+            #endregion
 
-            //           [count] = COUNT(*)
+            IList<Category> result = null;
+            IList<TreeNode> tree = null;
 
-            //    FROM krt_Naftan_orc_sapod AS knos INNER JOIN krt_Naftan AS kn
-
-            //        ON kn.KEYKRT = knos.keykrt
-
-            //    GROUP BY GROUPING SETS(
-            //        (kn.KEYKRT, kn.NKRT),
-            //        (kn.KEYKRT, kn.NKRT, knos.id_kart, knos.nkrt)
-            //	)
-            //)
-
-            //SELECT*
-            //FROM grSubResult
-            //WHERE[grNumber] <= 1
-            //ORDER BY KEYKRT DESC, id_kart desc;
-
-            //Assert
-            //var nodesHeap = new[] {
-            //    new TreeNode() {Id = 1 },
-            //    new TreeNode() {Id = 2, ParentId = 1 },
-            //    new TreeNode() {Id = 3, ParentId = 1 },
-            //    new TreeNode() {Id = 4, ParentId = 3 },
-            //    new TreeNode() {Id = 5 },
-            //};
             //Act
+            using (var dt = new DataTable()) {
+                using (var con = new SqlConnection(conectString))
+                using (var Adpt = new SqlDataAdapter(query, con)) {
+                    try {
+                        Adpt.Fill(dt);
+                    } catch (SqlException ex) {
+                        if (con.State == ConnectionState.Open) con.Close();
+                        Debug.WriteLine($"Test throws exception: {ex.Message}");
+                    } finally {
+                        if (con != null)
+                            if (con.State == ConnectionState.Open) con.Close();
+                        Adpt.Dispose();
+                    }
+                }
 
+                //Arrange
+                Assert.IsTrue(dt.Rows.Count > 0);
 
-            //Arrange
+                if (dt != null && dt.Rows.Count > 0) {
+                    result = dt.Select().ToList().Select(x => new Category {
+                        Id = Convert.ToInt32(x["elementId"]),
+                        CategoryName = x["levelName"].ToString(),
+                        Label = x["label"].ToString(),
+                        searchKey = Convert.ToInt64(x["searchkey"]),
+                        ParentId = Convert.ToInt32(x["parentId"]),
+                    }).ToList();
+                }
+            }
+
+            //fill tree
+            try {
+                tree = FillRecursive(result.ToList());
+            } catch (Exception) {
+                throw;
+            }
+
+            Assert.IsTrue(result.Count > 0);
+            Assert.IsTrue(tree.Count() == countGroup);
         }
+
+        public class Category {
+            public int Id { get; set; }
+            public string CategoryName { get; set; }
+            public string Label { get; set; }
+            public int? ParentId { get; set; }
+            public long searchKey { get; set; }
+        }
+
+        private static List<TreeNode> FillRecursive(List<Category> rows, int parentId = 0) {
+
+            var subResult = rows.Where(x => x.ParentId.Equals(parentId));
+
+            var result = subResult.Select(item => new TreeNode(item.CategoryName) {
+                Id = item.Id,
+                Children = FillRecursive(rows, item.Id)
+            }).ToList();
+
+            return result;
+        }
+
     }
 }
