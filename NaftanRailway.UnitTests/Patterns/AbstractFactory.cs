df34ba04ba78decd1eb515e9010aba87d412dce0
Pavel <allmantool@gmail.com>
2017-10-23 17:36:49 +0300

    23.10.2017

---
+++ NaftanRailway.UnitTests/Patterns/AbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace NaftanRailway.UnitTests.Patterns {
+    /// <summary>
+    /// Когда система не должна зависеть от способа создания и компоновки новых объектов
+    /// Когда создаваемые объекты должны использоваться вместе и являются взаимосвязанными
+    /// </summary>
+    class AbstractFactory {
+        public void AbstractFactoryMain() {
+
+        }
+    }
+}
---
+++ NaftanRailway.UnitTests/Patterns/FactoryMethod.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace NaftanRailway.UnitTests.Patterns {
+    /// <summary>
+    /// Когда заранее неизвестно, объекты каких типов необходимо создавать
+    /// Когда система должна быть независимой от процесса создания новых объектов и расширяемой: в нее можно легко вводить новые классы, объекты которых система должна создавать.
+    /// Когда создание новых объектов необходимо делегировать из базового класса классам наследникам
+    /// </summary>
+    class FactoryMethod {
+        public void FactoryMain() {
+            Developer dev = new PanelDeveloper("ООО КирпичСтрой");
+            House house2 = dev.Create();
+
+            dev = new WoodDeveloper("Частный застройщик");
+            House house = dev.Create();
+
+            Console.ReadLine();
+        }
+    }
+
+    // абстрактный класс строительной компании
+    abstract class Developer {
+        public string Name { get; set; }
+
+        public Developer(string n) {
+            Name = n;
+        }
+        // фабричный метод
+        abstract public House Create();
+    }
+
+    // строит панельные дома
+    class PanelDeveloper : Developer {
+        public PanelDeveloper(string n) : base(n) { }
+
+        public override House Create() {
+            return new PanelHouse();
+        }
+    }
+
+    // строит деревянные дома
+    class WoodDeveloper : Developer {
+        public WoodDeveloper(string n) : base(n) { }
+
+        public override House Create() {
+            return new WoodHouse();
+        }
+    }
+
+    abstract class House { }
+
+    class PanelHouse : House {
+        public PanelHouse() {
+            Console.WriteLine("Панельный дом построен");
+        }
+    }
+    class WoodHouse : House {
+        public WoodHouse() {
+            Console.WriteLine("Деревянный дом построен");
+        }
+    }
+}
