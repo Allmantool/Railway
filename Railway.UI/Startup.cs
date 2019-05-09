0ea72b44d9ba62a607d49604fb6c49e612d79bd4
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2019-05-09 23:25:24 +0300

    PR-Move UI project to AppClient app location

--- Railway.UI/Startup.cs
+++ Railway.UI/Startup.cs
@@ -1,33 +1,56 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Railway.UI
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
+      services.AddSpaStaticFiles(c =>
+      {
+        c.RootPath = "ClientApp/dist";
+      });
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      app.UseStaticFiles();
+      app.UseSpaStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(name: "default", template: "{controller}/{action=index}/{id}");
+      });
+
+      app.UseSpa(spa =>
+      {
+        // To learn more about options for serving an Angular SPA from ASP.NET Core,
+        // see https://go.microsoft.com/fwlink/?linkid=864501
+        spa.Options.SourcePath = "ClientApp";
+        if (env.IsDevelopment())
+        {
+          spa.UseAngularCliServer(npmScript: "start");
+        }
+      });
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+
+      app.UseDefaultFiles();
+    }
+  }
 }
