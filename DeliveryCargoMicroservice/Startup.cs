e3a2de411994742fea524e9ad29ef3409efee235
DESKTOP-9MCCCFM\User <allmantool@gmail.com>
2019-05-25 00:11:46 +0300

    DCM: Add swagger UI

--- DeliveryCargoMicroservice/Startup.cs
+++ DeliveryCargoMicroservice/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Railway.DeliveryCargo.API
 {
@@ -19,6 +20,16 @@ namespace Railway.DeliveryCargo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Delivery cargo API",
+                    Description = "ASP.NET Core Web API delivery cargo project"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +46,12 @@ namespace Railway.DeliveryCargo.API
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Delivery cargo API");
+            });
         }
     }
 }
